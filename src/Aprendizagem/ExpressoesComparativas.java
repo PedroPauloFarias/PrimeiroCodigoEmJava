84d5e25b440c9fe60b0f020873f18b93f67ad863
Seu Nome <seu@email.com>
2025-08-25 14:30:24 -0300

    Adicionando mais informações

---
+++ src/Aprendizagem/ExpressoesComparativas.java
@@ -0,0 +1,20 @@
+package Aprendizagem;
+
+public class ExpressoesComparativas {
+    public static void main(String[] args){
+
+    }
+}
+/**
+         São expressões que comparam
+ Ele vai mostrar se os valores são verdadeiros o falsos
+
+ Aqui temos alguns operadores compartivos:
+ >   maior
+ <   menor
+ >=  maior ou igual
+ <=  menor ou igual
+ ==  igual
+ !   diferente
+
+ */
