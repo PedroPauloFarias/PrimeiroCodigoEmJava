8948797dcad0ad6f37f48ccddcebd06a661cc5f2
Pedro Paulo <pedropaulocarvalhofarias@gmail.com>
2025-08-29 18:03:19 -0300

    Vendo a estrutura for

---
+++ src/Aprendizagem/Repeticao/ForPara.java
@@ -0,0 +1,19 @@
+package Aprendizagem.Repeticao;
+
+public class ForPara {
+    public static void main(String[] args){
+
+    }
+}
+/**
+             A estrutura de repetição de controle FOR/PARA
+
+Que é usada quando se sabe previamente
+a quantidade de repetições
+Sintaxe:
+ for (inicio; condição; incremento){
+    comando1
+    comando2
+ }
+
+ */
