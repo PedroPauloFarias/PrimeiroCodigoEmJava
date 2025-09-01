8e35b3cdec809a7e5ebecefe77f5984a1d9fc53d
Pedro Paulo <pedropaulocarvalhofarias@gmail.com>
2025-09-01 18:02:03 -0300

    For

--- src/Aprendizagem/Repeticao/ForPara.java
+++ src/Aprendizagem/Repeticao/ForPara.java
@@ -1,8 +1,21 @@
 package Aprendizagem.Repeticao;
 
+import java.util.Scanner;
+
 public class ForPara {
     public static void main(String[] args){
+        System.out.println("digite alguns numeros e a quantidade deles, e no final iremos somar ");
+        System.out.println("Quantas vezes você quer que digitar? ");
+    Scanner leitura = new Scanner(System.in);
+        int N = leitura.nextInt();
+        int soma = 0;
+        for(int i = 0; i < N; i++){ // O i++, esta abreviado, pois ele é assim i = i + 1
+            int x = leitura.nextInt();
+            soma = soma + x;
+        }
+        System.out.print("A soma dos numeros digitados é igual a " + soma );
 
+    leitura.close();
     }
 }
 /**
