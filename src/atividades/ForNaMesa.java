bdd75a4e6e2c480f4889f3c73f99da542df433e4
Pedro Paulo <pedropaulocarvalhofarias@gmail.com>
2025-09-04 17:54:16 -0300

    terminar

--- src/atividades/ForNaMesa.java
+++ src/atividades/ForNaMesa.java
@@ -1,13 +1,5 @@
 package atividades;
 
 public class ForNaMesa {
-    public static void main(String[] args){
-        int x = 3;
-        int y = 0;
-        for( int i = 0; i < x; i++){
-            System.out.print(i + " , ");
-            y = y + 5;
-            System.out.println(y);
-        }
-    }
+
 }
