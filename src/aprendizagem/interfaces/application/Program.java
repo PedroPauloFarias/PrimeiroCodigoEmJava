b13cc44f3456a6007a61cf548ce1b26defc5dde8
Pedro Paulo <p.farias@iesb.edu.br>
2026-01-13 13:39:05 -0300

    Fazendo

--- src/aprendizagem/interfaces/application/Program.java
+++ src/aprendizagem/interfaces/application/Program.java
@@ -1,4 +1,49 @@
 package aprendizagem.interfaces.application;
 
+import aprendizagem.interfaces.model.entities.CarRental;
+import aprendizagem.interfaces.model.entities.Vehicle;
+import aprendizagem.interfaces.model.services.BrazilTaxService;
+import aprendizagem.interfaces.model.services.RentalService;
+
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Locale;
+import java.util.Scanner;
+
 public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        DateTimeFormatter fmt = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+
+        System.out.println("Entre com os dados do aluguel");
+        System.out.print("Modelo do carro: ");
+        String carModel = sc.nextLine();
+        System.out.println("Retirada (dd/MM/yyyy hh:mm): ");
+        LocalDateTime start = LocalDateTime.parse(sc.nextLine(), fmt);
+        System.out.println("Retorno (dd/MM/yyyy hh:mm): ");
+        LocalDateTime finish = LocalDateTime.parse(sc.nextLine(), fmt);
+
+        CarRental cr = new CarRental(start, finish, new Vehicle(carModel)); // Aqui tem esse final pois a String esta no Vehicle e nao no CarRental
+
+
+        System.out.print("Entre com o preço por hora: ");
+        double pricePerHour = sc.nextDouble();
+        System.out.print("Entre com o preço por dia: ");
+        double pricePerDay = sc.nextDouble();
+
+        RentalService rentalService = new RentalService(pricePerDay, pricePerHour, new BrazilTaxService());
+
+        rentalService.processInvoice(cr);
+
+
+        System.out.print("FATURA: ");
+        System.out.println("Pagamento basico: " + String.format("%.2f", cr.getInvoice().getBasicPayment()));
+        System.out.println("Imposto: " + String.format("%.2f", cr.getInvoice().getTax()));
+        System.out.println("Pagamento total: " + String.format("%.2f", cr.getInvoice().getTotalPayment()));
+
+        sc.close();
+    }
 }
+
+// Aqui nao se utiliza interface
