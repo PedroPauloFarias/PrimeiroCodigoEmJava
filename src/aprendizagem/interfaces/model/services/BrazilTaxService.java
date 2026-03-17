b92d605d22dcc87191b430e9ab40ab47d44dc6d7
Pedro Paulo <p.farias@iesb.edu.br>
2026-03-17 17:52:55 -0300

    Nao lembro

--- src/aprendizagem/interfaces/model/services/BrazilTaxService.java
+++ src/aprendizagem/interfaces/model/services/BrazilTaxService.java
@@ -1,6 +1,6 @@
 package aprendizagem.interfaces.model.services;
 
-public class BrazilTaxService {
+public class BrazilTaxService  implements TaxService{
     public double tax(double amount) {
         if (amount <= 100.0) {
             return amount * 0.2;
@@ -9,3 +9,4 @@ public class BrazilTaxService {
         }
     }
 }
+// Se eu precisar mudar algo aqui eu nao mexo no programa principal, pois o programa principal esta atrelado ao TaxService
---
+++ src/aprendizagem/interfaces/model/services/TaxService.java
@@ -0,0 +1,5 @@
+package aprendizagem.interfaces.model.services;
+
+public interface TaxService {
+    double  tax(double amount);
+}
