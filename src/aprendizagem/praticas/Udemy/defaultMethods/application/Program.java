151a982d10e62d217f15afa9c178a3dface59d82
Pedro Paulo <p.farias@iesb.edu.br>
2026-06-05 16:30:18 -0300

    Primeiros passos

---
+++ src/aprendizagem/praticas/Udemy/defaultMethods/application/Program.java
@@ -0,0 +1,24 @@
+package aprendizagem.praticas.Udemy.defaultMethods.application;
+
+import aprendizagem.praticas.Udemy.defaultMethods.services.BrazilInterestService;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Amount: ");
+        double amount = sc.nextDouble();
+        System.out.print("Months: ");
+        int months = sc.nextInt();
+
+        BrazilInterestService is = new BrazilInterestService(2.0);
+        double payment = is.payment(amount, months);
+
+        System.out.println("Payment after " + months + "months");
+        System.out.println(String.format("%.2f", payment));
+    }
+}
