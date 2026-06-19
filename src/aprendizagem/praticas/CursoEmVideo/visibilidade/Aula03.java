bb183741aa90b2d36bb222b07a406ecf84b73a95
Pedro Farias <pedropaulocarvalhofarias@gmail.com>
2026-06-19 14:33:08 -0300

    Revendo os conceitos

---
+++ src/aprendizagem/praticas/CursoEmVideo/visibilidade/Aula03.java
@@ -0,0 +1,20 @@
+package aprendizagem.praticas.CursoEmVideo.visibilidade;
+
+public class Aula03 {
+    public static void main(String[] args) {
+        Caneta1 c1 = new Caneta1();
+        c1.cor = "Azul";
+        c1.ponta = 0.5f;
+        c1.tampar();
+
+        c1.status();
+        c1.rabiscar();
+
+        Caneta1 c2 = new Caneta1();
+        c2.modelo = " Bic";
+        c2.cor = " Preta";
+        c2.destampar();
+        c2.rabiscar();
+    }
+}
+
