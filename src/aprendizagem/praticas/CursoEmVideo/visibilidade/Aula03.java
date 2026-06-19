a7ec0195d804fd694d4a383c8d98495e949a5fed
Pedro Farias <pedropaulocarvalhofarias@gmail.com>
2026-06-19 15:09:16 -0300

    Estava tirando duvidas

--- src/aprendizagem/praticas/CursoEmVideo/visibilidade/Aula03.java
+++ src/aprendizagem/praticas/CursoEmVideo/visibilidade/Aula03.java
@@ -4,7 +4,7 @@ public class Aula03 {
     public static void main(String[] args) {
         Caneta1 c1 = new Caneta1();
         c1.cor = "Azul";
-        c1.ponta = 0.5f;
+//        c1.ponta = 0.5f;
         c1.tampar();
 
         c1.status();
