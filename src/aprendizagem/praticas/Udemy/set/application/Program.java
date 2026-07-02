1d387bf426a87985171aad350db512455d365e4b
Pedro Paulo <pedropaulocarvalhofarias@gmail.com>
2026-07-02 18:42:47 -0300

    OK

---
+++ src/aprendizagem/praticas/Udemy/set/application/Program.java
@@ -0,0 +1,23 @@
+package aprendizagem.praticas.Udemy.set.application;
+
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+public class Program {
+    public static void main(String[] args) {
+
+        Set<String> set = new LinkedHashSet<>();
+
+        set.add("TV");
+        set.add("Notebook");
+        set.add("Tablet");
+
+        set.removeIf(x -> x.length() >= 3);
+
+        for (String p : set) {
+            System.out.println(p);
+        }
+
+    }
+}
